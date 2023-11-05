dec41fe4b347ee69c6f04f702940f0fa061bbef9
Pavel <pzajtsev@yandex.ru>
2023-11-05 19:05:38 +0400

    initial commit

---
+++ src/main/java/com/too_codemen/Application.java
@@ -0,0 +1,12 @@
+package com.too_codemen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
